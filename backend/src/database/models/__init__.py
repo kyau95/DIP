c0b390910a645a7c090156628057416d0050d3d4
Kevin <kyau97@gmail.com>
2026-06-11 13:03:02 -0400

    setup session

---
+++ backend/src/database/models/__init__.py
@@ -0,0 +1,14 @@
+from .alert import Alert
+from .base_model import BaseModel
+from .price_history import PriceHistory
+from .product import Product
+from .scrape_job import ScrapeJob
+
+
+__all__ = [
+    "Alert",
+    "BaseModel",
+    "PriceHistory",
+    "Product",
+    "ScrapeJob"
+]
---
+++ backend/src/database/session.py
@@ -0,0 +1,22 @@
+from dotenv import dotenv_values
+from models import Product
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+CONF = dotenv_values()
+engine = create_engine(
+    CONF["DB_URL"],
+    echo=True    
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+)
+
+
+if __name__ == "__main__":
+    pass
