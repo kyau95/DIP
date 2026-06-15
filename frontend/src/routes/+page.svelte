2f6b6d89c529ffce2fc77b4d09aade9b5dec5d01
Kevin <kyau97@gmail.com>
2026-06-15 09:32:02 -0400

    base frontend table

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>This is a place holder</h1>
+<p>Welcome to the page</p>
+
+<table>
+  <tbody>
+    <tr>
+      <th>Item</th>
+      <th>Tracker Price</th>
+      <th>URL</th>
+    </tr>
+    <tr>
+      <td>RTX 5090</td>
+      <td>$599.99</td>
+      <td>A very long URL blah blah blah blah</td>
+    </tr>
+  </tbody>
+</table>
