2271b1ff20ba4f552f0490e6796c110087e8d7d9
Kevin <kyau97@gmail.com>
2026-06-15 16:30:20 -0400

    refactor table component

---
+++ frontend/src/lib/components/ProductTable.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  
+</script>
+
+<table>
+  <tbody>
+    <tr>
+      <th>Product Name</th>
+      <th>Tracker Price</th>
+      <th>URL</th>
+    </tr>
+    <tr>
+      <td>RTX 5090</td>
+      <td>$599.99</td>
+      <td>
+        <a href="https://example.com" rel="external">
+          A very long URL blah blah blah blah
+        </a>
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+<style>
+  table, th, td {
+    border: 1px solid;
+    border-collapse: collapse;
+    padding: 10px 0;
+    text-align: center;
+  }
+
+  table {
+    min-width: 800px;
+  }
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,39 +1,9 @@
 <script lang="ts">
-  
+  import ProductTable from "$lib/components/ProductTable.svelte";
 </script>
 
 <h1>This is a place holder</h1>
 <p>Welcome to the page</p>
 
-<table>
-  <tbody>
-    <tr>
-      <th>Product Name</th>
-      <th>Tracker Price</th>
-      <th>URL</th>
-    </tr>
-    <tr>
-      <td>RTX 5090</td>
-      <td>$599.99</td>
-      <td>
-        <a href="https://example.com" rel="external">
-          A very long URL blah blah blah blah
-        </a>
-      </td>
-    </tr>
-  </tbody>
-</table>
 
-
-<style>
-  table, th, td {
-    border: 1px solid;
-    border-collapse: collapse;
-    padding: 10px 0;
-    text-align: center;
-  }
-
-  table {
-    min-width: 800px;
-  }
-</style>
+<ProductTable />
