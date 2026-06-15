922f0db0fdaf5cfb1d672654e9cb8fc3dd9588e6
Kevin <kyau97@gmail.com>
2026-06-15 16:01:00 -0400

    basic table layout

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,17 +1,39 @@
+<script lang="ts">
+  
+</script>
+
 <h1>This is a place holder</h1>
 <p>Welcome to the page</p>
 
 <table>
   <tbody>
     <tr>
-      <th>Item</th>
+      <th>Product Name</th>
       <th>Tracker Price</th>
       <th>URL</th>
     </tr>
     <tr>
       <td>RTX 5090</td>
       <td>$599.99</td>
-      <td>A very long URL blah blah blah blah</td>
+      <td>
+        <a href="https://example.com" rel="external">
+          A very long URL blah blah blah blah
+        </a>
+      </td>
     </tr>
   </tbody>
 </table>
+
+
+<style>
+  table, th, td {
+    border: 1px solid;
+    border-collapse: collapse;
+    padding: 10px 0;
+    text-align: center;
+  }
+
+  table {
+    min-width: 800px;
+  }
+</style>
